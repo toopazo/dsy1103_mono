1910c53ecf3db89a3e633acd66cf45df9ca07ddd
Tomas Opazo <toopazo@proton.me>
2025-09-12 00:10:09 -0300

    Merge de dev a main

--- test_sample.py
+++ test_sample.py
@@ -1,9 +1,9 @@
-# from app.mvc_vista import Vista
+from app.mvc_vista import Vista
 
 # def test_vista():
 #     assert Vista().main() == "OK"
 
-from app.calculate import function_to_test
+# from app.calculate import function_to_test
 
 def test_vista_1():
     assert Vista().main() == "OK"
